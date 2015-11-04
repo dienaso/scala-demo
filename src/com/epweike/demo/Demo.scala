e88bd9b13028bd12aa23c52e03e9fb1c8d244fd7
ping199143 <89768641@qq.com>
2015-11-04 16:09:32 +0800

    初始提交

---
+++ src/com/epweike/demo/Demo.scala
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2010-2015 epweike.com.
+ * @Description: TODO
+ * @author 吴小平
+ * @date Nov 4, 2015 10:06:15 AM
+ *
+ */
+package com.epweike.demo
+
+/**
+ * @author Administrator
+ *
+ */
+object Demo {
+  def main(args: Array[String]) {
+	  println("hello world")
+	  println("hello world")
+  }
+
+}
